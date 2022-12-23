07d8785eca0e78ebf91cbec8abc02fec7e899e77
vaagdevi <vaagdevi@DESKTOP-JLM4JIR>
2022-12-23 01:40:43 -0800

    hellotask

---
+++ hellotask/src/hellotask/drivercls.java
@@ -0,0 +1,11 @@
+package hellotask;
+
+public class drivercls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("dinesh kumar");
+
+	}
+
+}
---
+++ hellotask/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vaagdevi
+ *
+ */
+module hellotask {
+}
